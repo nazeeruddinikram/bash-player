0e198d971dbed2971aa8903164a138694455667a
levi <levi0x0x@gmail.com>
2014-01-29 00:28:14 +0200

    v0.4

--- bmplayer.sh
+++ bmplayer.sh
@@ -1,60 +1,107 @@
-#!/bin/sh
-#bashPlayer (bmplayer) A simple Video player based on mplayer
-#Written by levi0x0
-#license: GPL3
-#bashPlayer 21/01/2014 - 16:26:30
-#tested on: ArchLinux, Debian
+#!/bin/bash
+#  bmplayer.sh
+#  
+#  Copyright 2014 levi (https://github.com/levi0x0/bash-player)
+#  
+#  bash-player is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  bash-player is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
 
 #version
-version = 0.3
+version=0.4
 
-#movies folder (change this if you need)
-movies="/home/$USER/Videos/"
+#videos folder
+folder="/home/$USER/Videos"
 
-#enable utf8 subtitles
-#echo "utf8=true" >> ~/.mplayer/config
+#date
+date="22-01-2014"
 
-#zenity installed?
-if which mplayer 2>/dev/null; then
-	echo "[DEBUG] Found zenity"
+#mplayer options (you can add)
+mplayerpa="-fs"
+
+#status var
+debug="[DEBUG]"
+error="[ERROR]"
+exit="[EXIT]"
+
+#exit in the and 0 for yes 1 for no
+exitend=1
+
+#enable mplayer utf8 support
+#echo utf8=true >> ~/.mplayer/config
+
+#check if zenity installed
+if which zenity 2> /dev/null ;then 
+	echo "$debug Found zenity"
 else
-	zenity --error --title="zenity not installed!" --text="Please install the 'zenity' package"
-	exit;
+	echo "$error Zenity package not installed"
 fi
-#mplayer installed?
-if which mplayer 2>/dev/null; then
-	echo "[DEBUG] Found mplayer"
+#check if mplayer installed
+if  which mplayer 2> /dev/null ;then 
+	echo "$debug Found mplayer"
 else
-	zenity --error --title="mplayer not installed!" --text="Please install the 'mplayer' package"
-	exit;
-fi
-#startup dialog
-zenity --forms --title="Bmplayer - $version" --text="bash-player - A simple GUI for Mplayer\n\n\t\tWritten by levi0x0\n\nVersion 1.0.3\nLicense: GPL3\n\n\t\t\tEnjoy (:"  --ok-label="Select video" --cancel-label="Exit"
-if [ $? -ne 0 ]; then 
-	echo "[DEBUG] exit"
+	zenity --error --title="mplayer not found" --text="mplayer package not installed"
 	exit
-else
-	echo "[DEBUG] Select video"
 fi
-#choose video
-video=`zenity --file-selection --title="Select video" --filename="$movies"`
-if [ $? -ne 0 ]; then
-	echo "[DEBUG] File not selected."
-	exit
-else
-	echo "[DEBUG] Found Video"
-fi
-#ask for subtitles
-zenity --question --title="Add subtitles"  --text="Add Subtitles?"  --ok-label="add" --cancel-label="play video"
-if [ $? -ne 0 ]; then
-	mplayer -fs "$video" # -fs for full screen
-else
-	subs=`zenity --file-selection --title="Select subtitles" --filename="$movies"`
-	if [ $? -ne 0 ]; then
-		echo "[DEBUG] Not Selected."
-		exit
+
+	
+dialog() {
+	zenity  --question \
+	--title="Bmplayer - $version" \
+	--ok-label="Select video" \
+	--cancel-label="Exit"
+	#test 1
+	if [ $?  -ne 0 ]; then 
+		echo "$exit Canceld."
+		exit #exit
 	else
-		mplayer -fs "$video" -sub "$subs"
+		#select video
+		video=`zenity --file-selection --title="Select video" --filename=$folder`
+		if [ $? -ne 0 ]; then
+			echo "$debug no File selected."
+			exit #exit
+		else
+			zenity --question --title="add subtitles"  --text="Want to add Subtitles?"  --ok-label="Yes, add" --cancel-label="No, play video"
+			if [ $? -ne 0 ]; then
+				mplayer $mplayerpa "$video"
+				if [ $exitend -eq 0 ];then
+					dialog
+				else
+					exit
+				fi
+			#else select subtitles
+			else
+				subtitles=`zenity --file-selection --title="Select subtitles" --filename=$folder`
+				#run mplayer 
+				mplayer $mplayerpa "$video" -sub  "$subtitles"
+				if [ $exitend -eq 0 ]; then
+					dialog
+				else
+					exit
+				fi
+			fi
+		fi
 	fi
+}
+
+#check for command line args 
+if [ -z $1 ];then
+	echo "$debug No CLA"
+	dialog
+else
+	video="$1"
+	mplayer $mplayerpa "$video"
 fi
-#done share the c0de! by levi0x0
