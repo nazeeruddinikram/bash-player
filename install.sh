0f6e09cd267f6e1b85067f084d4b45a1d60a91f5
levi0x0 <levi0x0x@gmail.com>
2014-05-02 19:31:43 +0300

    mv changed to install :penguin:

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@
 #
 # Install bash-player to Your System
 # Author: levi (levi0x0) "http://github.com/levi0x0/bash-player"
-# Version: 0.1
-# Date: 11-04-2014
+# Version: 0.2
+# Date: 02-05-2014
 
 #bin dir for bmplayer.sh
 bin="/usr/bin/"
@@ -16,7 +16,7 @@ pixmaps="/usr/share/pixmaps/"
 desktop="/usr/share/applications"
 
 #version
-version="0.1"
+version="0.2"
 
 #status variables
 debug="[DEBUG]"
@@ -27,7 +27,7 @@ exit="[EXIT]"
 banner="Bash-player Installer $version Copyright 2014 (C) levi (levi0x0)"
 
 installbp() {
-	echo -e "\tBash-Player Installer"
+	echo -e "\t[Bash-Player Installer]"
 	if [ $UID != 0 ];then
 		echo "$debug Please run as root! we need to move files."
 		echo -e "Look at the source! (:\n"
@@ -37,25 +37,23 @@ installbp() {
 	fi
 	#moving bmplayer.sh to /usr/bin
 	echo "$debug Moving bmplayer.sh.."
-	mv bmplayer.sh $bin/bmplayer
-	chmod +x $bin/bmplayer
+	install -Dm755 bmplayer.sh $bin/bmplayer
 	#moving the icon tho pixmaps
 	echo "$debug Moving bash-player.png.. to $pixmaps"
-	mv bash-player.svg $pixmaps
+	install -Dm755 bash-player.svg $pixmaps
 	#moving desktop 
 	echo "$debug Moving bash-player.desktop.. to $desktop"
-	mv bash-player.desktop $desktop
-    #moving bmplayer_sh
-    echo "$debug Moving mplayer_config.sh to $bin"
-    mv mplayer_config.sh $bin/mplayer_config
-    chmod +x $bin/mplayer_config
+	install -Dm755  bash-player.desktop $desktop
+    	#moving bmplayer_sh
+    	echo "$debug Moving mplayer_config.sh to $bin"
+    	install -Dm755 mplayer_config.sh $bin/mplayer_config
 	echo -e "\nOK! Bash-player installed.!"
 	echo "Use your Application menu to start bash-player"
 	echo -e "Or run 'bmplayer/mplayer_config' from the terminal..\n" 
 }
 
 uninstallbp() {
-	echo -e "\tBash-Player uninstaller"
+	echo -e "\t[Bash-Player uninstaller]"
 	if [ $UID != 0 ];then
 		echo "$debug Please run as root! we need to remove files."
 		echo -e "Look at the source! (:\n"
@@ -72,11 +70,10 @@ uninstallbp() {
 	#removing desktop 
 	echo "$debug removing bash-player.desktop.."
 	rm -f $desktop/bash-player.desktop
-    #removing mplayer_config.sh
-    echo "$debug removing mplayer_config.sh.."
-    rm -f $bin/mplayer_config
+    	#removing mplayer_config.sh
+    	echo "$debug removing mplayer_config.sh.."
+    	rm -f $bin/mplayer_config
 	echo -e "\nOK! Bash-player Not installed.!"
-	echo -e "Written by levi (http://github.com/levi0x0)\n"
 }
 
 if [ -z $1 ]; then
@@ -86,8 +83,8 @@ if [ -z $1 ]; then
 	echo -e "\tTo uninstall: $0 uninstall"
 	echo -e "\nThat's it! (:"
 	exit
-elif [[ $1 == "install" ]];then
+elif [[ $1 == "install" ]] || [[ $1 == "i" ]];then
 	installbp
-elif [[ $1 == "uninstall" ]];then 
+elif [[ $1 == "uninstall" ]] || [[ $1 == "u" ]];then 
 	uninstallbp
 fi
